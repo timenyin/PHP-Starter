ad47f0701a5d9dee5de7db62ccec42dc3bc7cc2d
Harmony <47876447+timenyin@users.noreply.github.com>
2024-02-29 03:23:24 +0100

    countnation

--- 02-arrays-and-iteration/06-basic-loops/index.php
+++ 02-arrays-and-iteration/06-basic-loops/index.php
@@ -1,3 +1,10 @@
+<?php
+// for ($i = 0; $i <= 10; $i++) {
+//     echo $i . '<br />';
+// }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,18 +12,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>PHP From Scratch</title>
+    <title>06-basic-loops</title>
 </head>
 
 <body class="bg-gray-100">
     <header class="bg-blue-500 text-white p-4">
         <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
+            <h1 class="text-3xl font-semibold">Basic-loops</h1>
         </div>
     </header>
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
             <!-- Output -->
+            <ul>
+                <?php for ($i = 0; $i <= 10; $i++) : ?>
+                    <li>Number:
+                        <? $i ?>
+                    </li>
+                <?php endfor; ?>
+            </ul>
         </div>
     </div>
 </body>
