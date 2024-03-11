2259a9e7ce70522461cf6ea28f480dd9f8815f38
Harmony <47876447+timenyin@users.noreply.github.com>
2024-03-11 23:42:11 +0100

    next video comparison logic 28

--- 03-control-structures/01-if-statements/index.php
+++ 03-control-structures/01-if-statements/index.php
@@ -1,10 +1,27 @@
 <?php
-
+$age = 21;
 // If statement
-
+/*
+if ($age >= 19) {
+    echo 'you are old enough to drink';
+} else {
+    echo 'you are not old enough to drink';
+}
+*/
 // If-Else
+echo '<br>';
 
 // Nested if statement
+// if ($age  <= 21) {
+//     echo 'you are old enough to drink and vote';
+// } else {
 
+if ($age >= 21) {
+    echo 'you can vote and drink  in US';
+} else if ($age >= 18) {
+    echo 'you are Not old enough to vote in the  US';
+} else {
+    echo "you can't not from US";
+}
+//}
 // If-Else-If
-
--- 03-control-structures/02-conditional-html-output/index.php
+++ 03-control-structures/02-conditional-html-output/index.php
@@ -1,9 +1,16 @@
+<?php
+
+$isloggedIn = true;
+$name = 'Null';
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="../../main.ico" type="image/x-icon" />
   <script src="https://cdn.tailwindcss.com"></script>
   <title>PHP From Scratch</title>
 </head>
@@ -17,7 +24,26 @@
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
       <!-- Output -->
-      <h1 class="text-3xl">Welcome</h1>
+      <?php
+      if ($isloggedIn) : ?>
+        <?php if (isset($name)) : ?>
+          <h1 class="text-3xl"> Welcome to the App <?= $name ?> </h1>
+        <?php else : ?>
+          <h1 class="text-3xl"> Welcome to the App</h1>
+        <?php endif ?>
+      <?php else : ?>
+        <h1 class="text-3xl"> Logged In to view</h1>
+      <?php endif ?>
+
+
+      <?php if ($isloggedIn && $name) : ?>
+        <h1 class="text-3xl"> Welcome to the App <?= $name ?> </h1>
+      <?php elseif ($isloggedIn) : ?>
+        <h1 class="text-3xl"> Welcome to the App</h1>
+      <?php else : ?>
+        <h1 class="text-3xl"> Logged In view </h1>
+      <?php endif ?>
+
     </div>
   </div>
 </body>
